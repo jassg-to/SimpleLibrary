8ed9f3df96e8dcde8e0f8e7f217a392c60046053
unknown <lfelipecamargo@gmail.com>
2019-03-09 01:49:12 -0500

    initial commit adding member model

---
+++ controllers/memberController.js
@@ -0,0 +1,206 @@
+var Member = require('../models/member');
+
+var async = require('async');
+
+
+// exports.index = function (req, res) {
+//   async.parallel({
+//     book_count: function (callback) {
+//       Book.count(callback);
+//     },
+//     book_instance_count: function (callback) {
+//       BookInstance.count(callback);
+//     },
+//     book_instance_available_count: function (callback) {
+//       BookInstance.count({ status: 'Available'}, callback);
+//     },
+//     author_count: function (callback) {
+//       Author.count(callback);
+//     },
+//     genre_count: function (callback) {
+//       Genre.count(callback);
+//     },
+//   }, function (err, results) {
+//     res.render('index', { title: 'Local Library Home', error: err, data: results })
+//   });
+// };
+
+// Display list of all books
+exports.member_list = function (req, res, next) {
+  Member.find({}, 'first_name last_name ')
+    .exec(function (err, list_members) {
+      if (err) return next(err);
+      res.render('member_list', { title: 'Member List', member_list: list_members });
+    });
+};
+
+// Display detail page for a specific member
+exports.member_detail = function (req, res) {
+  Member.findById(req.params.id)
+  .exec(function(err, memberInstance) {
+    if (err) return next(err);
+    res.render('member_detail', {title: 'Member Detail', member: memberInstance});
+  });
+};
+
+// Display member create form on GET
+exports.member_create_get = function (req, res, next) {
+  res.render('member_form', { title: 'Create Member'});
+};
+
+// Handle member create on POST
+exports.member_create_post = function (req, res, next) {
+
+  sanitizeRequest(req);
+  validateRequest(req);
+
+  var member = createMemberFromRequest(req);
+  console.log('member: ' + member);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    // Some problems so we need to re-render our member
+    console.log('GENRE: ' + req.body.genre);
+
+    console.log('ERRORS: ' + errors);
+    
+    res.render('member_form', { title: 'Create member', member: member, errors: errors });
+
+  } else {
+    // Data from form is valid.
+    // We could check if member exists already, but lets just save.
+
+    member.save(function (err) {
+      if (err) { return next(err); }
+      //successful - redirect to new member record.
+      res.redirect(member.url);
+    });
+  }
+};
+
+
+exports.member_delete_get = function (req, res, next) {
+
+  async.parallel({
+    member: function (callback) {
+      Member.findById(req.params.id).exec(callback)
+    }
+  }, function (err, results) {
+    if (err) { return next(err); }
+    //Successful, so render
+    res.render('member_delete', { title: 'Delete member', member: results.member});
+  });
+
+};
+
+// Handle member delete on POST
+exports.member_delete_post = function (req, res, next) {
+
+  //Assume the post will have id (ie no checking or sanitisation).
+
+  async.parallel({
+    member: function (callback) {
+      Member.findById(req.params.id).exec(callback)
+    },
+    // book_bookinstances: function (callback) {
+    //   BookInstance.find({ 'book': req.params.id }).exec(callback)
+    // },
+  }, function (err, results) {
+    if (err) { return next(err); }
+    //Success
+    // if (results.book_bookinstances > 0) {
+    //   //Book has book_instances. Render in same way as for GET route.
+    //   res.render('book_delete', { title: 'Delete Book', book: results.book, book_instances: results.book_bookinstances });
+    //   return;
+    // }
+    // else {
+      //Book has no bookinstances. Delete object and redirect to the list of books.
+      Member.findByIdAndRemove(req.body.id, function deleteMember(err) {
+        if (err) { return next(err); }
+        //Success - got to books list
+        res.redirect('/catalog/members')
+      })
+
+    // }
+  });
+
+};
+
+// Display book update form on GET
+exports.member_update_get = function(req, res, next) {
+
+  sanitizeId(req);
+
+  Member.findById(req.params.id, function (err, member) {
+    if (err) { return next(err); }
+    //On success
+    res.render('member_form', { title: 'Update Member', member: member });
+  });
+ 
+};
+
+// Handle member update on POST
+exports.member_update_post = function (req, res, next) {
+  
+  sanitizeRequest(req);
+  validateRequest(req);
+
+  var member = createMemberFromRequest(req);
+
+  var errors = req.validationErrors();
+  if (errors) {
+    //If there are errors render the form again, passing the previously entered values and errors
+    res.render('member_form', { title: 'Update Member', member: member, errors: errors });
+    return;
+  } else {
+    // Data from form is valid. Update the record.
+    Member.findByIdAndUpdate(req.params.id, member, {}, function (err, themember) {
+      if (err) { return next(err); }
+      //successful - redirect to genre detail page.
+      res.redirect(themember.url);
+    });
+  }
+};
+
+sanitizeRequest = function (req) {
+  req.sanitize('first_name').escape();
+  req.sanitize('last_name').escape();
+  req.sanitize('email').escape();
+  req.sanitize('phone').escape();
+  req.sanitize('since').trim();
+  req.sanitize('last_renew').toDate();
+  req.sanitize('code').trim();
+
+  sanitizeId(req);
+}
+
+sanitizeId = function (req){
+  //Sanitize id passed in. 
+  req.sanitize('id').escape();
+  req.sanitize('id').trim();
+}
+
+validateRequest = function (req) {
+  //Check other data
+  req.checkBody('first_name', 'First name must not be empty.').notEmpty();
+  req.checkBody('last_name', 'Last name must not be empty').notEmpty();
+  req.checkBody('phone', 'Phone must not be empty').notEmpty();
+  req.checkBody('since', 'Since must not be empty').notEmpty();
+  req.checkBody('last_renew', 'Last renew must not be empty').notEmpty();
+  req.checkBody('code', 'Code must not be empty').notEmpty();
+}
+
+//create an instance of member from request
+createMemberFromRequest = function (req) {
+  return new Member(
+    {
+      first_name: req.body.first_name,
+      last_name: req.body.last_name,
+      email: req.body.email,
+      phone: req.body.phone,
+      since: req.body.since,
+      last_renew: req.body.last_renew,
+      code: req.body.code,
+      _id: req.params.id || undefined//This is required, or a new ID will be assigned!
+    });
+}
---
+++ models/member.js
@@ -0,0 +1,28 @@
+var mongoose = require('mongoose');
+var moment = require('moment')
+var Schema = mongoose.Schema;
+
+var MemberSchema = Schema ({
+  first_name: { type: String, required: true},
+  last_name: { type: String, required: true},
+  email: { type: String, required: false},
+  phone: { type: String, required: true},
+  since: { type: String, required: true},
+  last_renew: { type: Date, required: true},
+  code: { type: String, required: true}
+});
+
+MemberSchema
+.virtual('url')
+.get(function (){
+  return '/catalog/member/' + this._id;
+});
+
+MemberSchema
+.virtual('last_renew_formatted')
+.get(function () {
+return moment(this.last_renew).format('YYYY-MM-DD');
+});
+
+
+module.exports = mongoose.model('Member', MemberSchema)
